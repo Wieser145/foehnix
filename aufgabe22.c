d96c4e67d554e00780b622541bb13622894b9804
Wieser145 <61455779+Wieser145@users.noreply.github.com>
2020-10-30 15:58:49 +0100

    Create aufgabe22.c
    Aufgabe Nr 3

---
+++ aufgabe22.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+
+int main ()
+{
+     
+    int a = 2;
+    char b[] = "H";
+    int c = a + b;
+
+    printf ("Sum of two numbers : %d \n", c);
+
+
+}
+
+//6422292 mit Hello = 5
+//6422297 mit '' = 0
+// 6422296 mit H = 1
+//6422290 mit Hellorr = 7
+//6422289
+//0 1 
+//2^32 = 65536
+//2^x = 6422297 
+//ln2(6422297)
